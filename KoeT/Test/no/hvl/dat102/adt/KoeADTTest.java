d79c015aba145dfef6998110653a7ca19a882cc2
h599006 <jonas.b.engen@icloud.com>
2022-01-25 10:50:07 +0100

    commit

--- KoeT/Test/no/hvl/dat102/adt/KoeADTTest.java
+++ KoeT/Test/no/hvl/dat102/adt/KoeADTTest.java
@@ -1,5 +1,34 @@
 package no.hvl.dat102.adt;
 
-public class KoeADTTest {
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public abstract class KoeADTTest {
+	
+	private KoeADT<Integer> koe;
+	
+	private Integer e0 = 1;
+	private Integer e1 = 2;
+	private Integer e2 = 3;
+	private Integer e3 = 4;
+	private Integer e4 = 5;
+	
+	protected abstract KoeADT<Integer> reset();
+	
+	@BeforeEach
+	public void setup() {
+		koe = reset();
+	}
+	
+	@Test
+	public void nyStabelErTom() {
+		assertTrue(koe.erTom());
+	}
+	
+	@Test
+	public void testInnKoe(T element) {
+		
+	}
 }
