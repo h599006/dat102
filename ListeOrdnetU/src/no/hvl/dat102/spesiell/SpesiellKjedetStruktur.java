b47ba0eee27f20ca45d6a9b2d4bd96a4e663f6ba
h599006 <jonas.b.engen@icloud.com>
2022-02-10 11:37:04 +0100

    commit

--- ListeOrdnetU/src/no/hvl/dat102/spesiell/SpesiellKjedetStruktur.java
+++ ListeOrdnetU/src/no/hvl/dat102/spesiell/SpesiellKjedetStruktur.java
@@ -54,6 +54,16 @@ public class SpesiellKjedetStruktur<T> {
 					// kjedete strukturen.
 		
 		LinearNode<T> r, n, s;
+		s = start;
+		r = null;
+		
+		while (s != null) {
+			n = s;
+			s = s.getNeste();
+			n.setNeste(r);
+			r = n;
+		}
+		start = r;
 	}// metode
 
 	public int finnAntallLike(T el) {
